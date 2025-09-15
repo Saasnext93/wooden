a403c4778c71a6dcf265c7ef00b450b03a86bfbe
Deepak Bagada <saasnextdb@gmail.com>
2025-09-15 16:45:40 +0000

    lets make the 4 vertical image banner with hover effect in each image (_

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutSection from "@/components/landing/AboutSection";
 import ContactForm from "@/components/landing/ContactForm";
 import FeaturedProducts from "@/components/landing/FeaturedProducts";
 import HeroSection from "@/components/landing/HeroSection";
+import ImageBannerGrid from "@/components/landing/ImageBannerGrid";
 import ManufacturingProcess from "@/components/landing/ManufacturingProcess";
 import NewProducts from "@/components/landing/NewProducts";
 import OurPhilosophy from "@/components/landing/OurPhilosophy";
@@ -13,6 +14,7 @@ export default function Home() {
       <HeroSection />
       <NewProducts />
       <OurPhilosophy />
+      <ImageBannerGrid />
       <FeaturedProducts />
       <AboutSection />
       <ManufacturingProcess />
