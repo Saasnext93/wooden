85b2c02e3026c3e2186e8f2f5b36488847a79b7e
Deepak Bagada <saasnextdb@gmail.com>
2025-09-22 18:46:33 +0000

    Please update the image here to the newly chosen ones (_for element img_

--- src/components/landing/KitchenLayouts.tsx
+++ src/components/landing/KitchenLayouts.tsx
@@ -30,7 +30,7 @@ const layouts = [
   },
   {
     name: 'Island',
-    imageUrl: '/island-kitchen.jpg',
+    imageUrl: 'https://images.unsplash.com/photo-1586208958839-06c17cacdf08?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxM3x8bW9kdWxhciUyMGtpdGNoZW58ZW58MHx8fHwxNzU4NTY2Mzg4fDA&ixlib=rb-4.1.0&q=80&w=1080',
     imageHint: 'island kitchen layout',
   },
 ];
